4060f558cbcf921b6331fbab7fba37f829f716e6
Pratyush Kamal <pratyushkamal88@gmail.com>
2024-06-03 23:21:31 +0530

    strings

---
+++ DSA/strings/test1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    string str1 = "pratyush";
+    string str2("kamal");
+
+    cout<<str1<<" "<<str2<<endl;
+
+    return 0;
+}
