28a76a67c05c34f65dad71c568d4866fac091e4d
Pratyush Kamal <pratyushkamal88@gmail.com>
2024-06-04 00:11:45 +0530

    inputoutput

---
+++ DSA/strings/inputOutput.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    string strr;
+    cout<<"Enter string: ";
+    cin>>strr;
+    cout<<"You entered: "<<strr<<endl;
+
+    return 0;
+}
